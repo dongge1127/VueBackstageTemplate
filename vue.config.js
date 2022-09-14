c9b0ed15dd5e0d0e42f8f842d79eeee7312e9aad
莫根东 <hn.mogendong@aisino.com>
2022-09-14 09:47:00 +0800

    feat: 修改vue.config.js，添加项目基础路径、打包目录、路径别名、mock等配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,47 @@
+"use strict";
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+const port = process.env.port || process.env.npm_config_port || 6699; // dev port
+
+const name = "my vue template";
+
 module.exports = {
-  transpileDependencies: true,
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  outputDir: process.env.VUE_APP_OUTPUT_DIR,
+  assetsDir: "static",
+  lintOnSave: process.env.NODE_ENV === "development",
+  productionSourceMap: false,
+  devServer: {
+    port,
+    // proxy: {
+    //   "/gateway-invoice": {
+    //     target: process.env.VUE_APP_BASE_API,
+    //     ws: true,
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       "/gateway-invoice": "",
+    //     },
+    //   },
+    // },
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    before: require("./mock/mock-server.js"), // webpack3
+    // onBeforeSetupMiddleware: require("./mock/mock-server.js"), // webpack4
+    // setupMiddlewares: require("./mock/mock-server.js"), // webpack5
+  },
+  configureWebpack: {
+    name: name,
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        api: resolve("src/api"),
+      },
+    },
+  },
 };
