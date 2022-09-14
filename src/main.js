27927c2f7acb3a4f550c0065712f8ea1269ee668
莫根东 <hn.mogendong@aisino.com>
2022-09-14 09:58:20 +0800

    feat: 入口文件，命令及依赖配置文件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "@/styles/index.scss";
+import "@/permission";
+
+import ElementUI from "element-ui"; // 引入组件
+import "element-ui/lib/theme-chalk/index.css"; // 组件样式
+import locale from "element-ui/lib/locale/lang/zh-CN"; // 中文字体包
+Vue.use(ElementUI, { locale });
+
 Vue.config.productionTip = false;
 
 new Vue({
